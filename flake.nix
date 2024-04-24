1fb68b8b9ab307053d689c2d408371a664d957b1
Mutsuha Asada <me@momee.mt>
2024-04-24 06:06:05 +0000

    fix: add cmd arguments to alejandra

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nil
+            alejandra
+            sops
+            opentofu
+            terraform-ls
+          ];
+        };
+      }
+    );
+}
