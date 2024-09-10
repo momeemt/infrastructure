c5a67bf79899625ca7a3e9054fc08d3e86e0653d
Mutsuha Asada <me@momee.mt>
2024-09-10 13:58:57 +0000

    change: skip validations on terraform

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,10 +3,14 @@ terraform {
 
   backend "s3" {
     bucket = "terraform"
-    key = "terraform.tfstate"
+    key    = "terraform.tfstate"
     endpoints = {
       s3 = "https://minio.momee.mt/"
     }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
   }
 }
 
